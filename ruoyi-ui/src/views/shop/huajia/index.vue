5129500e41e0e815a80233ee363ab5bb79c9ba98
zhoulq <1044145649@qq.com>
2023-05-26 09:07:20 +0800

    删除多余代码

--- ruoyi-ui/src/views/shop/huajia/index.vue
+++ ruoyi-ui/src/views/shop/huajia/index.vue
@@ -448,27 +448,6 @@ export default {
       })
     }
   },
-    // data () {
-    //   return {};
-    // },
-    // initCharts () {
-    //   let myChart = this.$echarts.init(this.$refs.chart);
-    //   console.log(this.$refs.chart)
-    //   // 绘制图表
-    //   myChart.setOption({
-    //     title: { text: '在Vue中使用echarts' },
-    //     tooltip: {},
-    //     xAxis: {
-    //       data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-    //     },
-    //     yAxis: {},
-    //     series: [{
-    //       name: '销量',
-    //       type: 'bar',
-    //       data: [5, 20, 36, 10, 10, 20]
-    //     }]
-    //   });
-    // }
   mounted () {
     console.log(this.chartData);
     this.initCharts();
